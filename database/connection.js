832a39a499c8e73cbb97546065af38cd7d6e7665
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-03-25 21:29:35 -0300

    instalando banco de dados

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,11 @@
 const { Sequelize, DataTypes } = require("sequelize")
+const path = require("path")
+require("dotenv").config({ path: path.join(__dirname, "../.env") })
 const sequelize = new Sequelize({
-    database: "ycaijoal",
-    username: "ycaijoal",
-    password: "AqY1N4jTNF3AAXUSz4a7VUA0RJi2MOAr",
-    host: "isabelle.db.elephantsql.com",
-    dialect: "postgres"
+    database: process.env.DATA_BASE,
+    username: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
 })
 module.exports = { sequelize, DataTypes }
